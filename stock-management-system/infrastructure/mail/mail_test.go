da1d560f7e75f07153a8a330a552901874cf4b3d
shinbunbun <takamuneyuto@gmail.com>
2021-10-17 20:19:43 +0900

    add ses test

---
+++ stock-management-system/infrastructure/mail/mail_test.go
@@ -0,0 +1,33 @@
+package mail
+
+import "testing"
+
+func TestSendMail(t *testing.T) {
+	type args struct {
+		message   string
+		recipient string
+		subject   string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "success case",
+			args: args{
+				message:   "test message",
+				recipient: "success@simulator.amazonses.com",
+				subject:   "test subject",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := SendMail(tt.args.message, tt.args.recipient, tt.args.subject); (err != nil) != tt.wantErr {
+				t.Errorf("SendMail() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
