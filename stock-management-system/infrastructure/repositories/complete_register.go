f2f8e76ee995e993769129e3e5d72f880c962e69
shinbunbun <takamuneyuto@gmail.com>
2021-10-21 16:03:02 +0900

    add complete_register and implement Check, Delete Code

---
+++ stock-management-system/infrastructure/repositories/complete_register.go
@@ -0,0 +1,40 @@
+package repositories
+
+import (
+	"errors"
+
+	"github.com/Yuto/ubic-stock-management-api/stock-management-system/infrastructure/database"
+)
+
+type CompleteRepository struct {
+	UbicFoodHandler database.UbicFoodHandler
+}
+
+func (r *CompleteRepository) CheckCode(code string) (string, error) {
+	widgetArr, err := r.UbicFoodHandler.GetByID(code)
+	if err != nil {
+		return "", err
+	}
+	email := ""
+	for i := 0; i < len(widgetArr); i++ {
+		if widgetArr[i].DataKind == "code-email" {
+			email = widgetArr[i].Data
+		}
+	}
+	if email == "" {
+		return "", errors.New("cannot find email address")
+	}
+	return email, nil
+}
+
+func (r *CompleteRepository) DeleteCode(code string) error {
+	err := r.UbicFoodHandler.DeleteByID(code)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+/* func (r *CompleteRepository) RegisterUser(email) (string, error) {
+
+} */
