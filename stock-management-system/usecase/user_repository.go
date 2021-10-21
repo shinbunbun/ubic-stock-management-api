30b312a7ea665778d6be9b9b30a4c845fb01534b
endered <yy56ga10ve@gmail.com>
2021-10-22 03:50:15 +0900

    move user repository definition

--- stock-management-system/usecase/user_repository.go
+++ stock-management-system/usecase/user_repository.go
@@ -8,3 +8,49 @@ type UserRepository interface {
 	Delete(string) error
 	Create(string, string, string) (string, error)
 }
+
+const (
+	UserNotFoundError   = UserRepositoryErr("User Not Found Error")
+	CantDeleteUserError = UserRepositoryErr("Can't delete user, because there are no correspond user")
+	FailedCreateUser    = UserRepositoryErr("Can't create user, because your email was already used")
+)
+
+type UserRepositoryErr string
+
+func (e UserRepositoryErr) Error() string {
+	return string(e)
+}
+
+func (it *Interactor) FindUserByID(id string) (domain.User, error) {
+	return it.UserRepository.FindByID(id)
+}
+
+func (it *Interactor) FindUserByEmail(email string) (domain.User, error) {
+	return it.UserRepository.FindByEmail(email)
+}
+
+func (it *Interactor) DeleteUserByID(id string) error {
+	_, err := it.UserRepository.FindByID(id)
+
+	switch err {
+	case UserNotFoundError:
+		return CantDeleteUserError
+	case nil:
+		return it.UserRepository.Delete(id)
+	default:
+		return err
+	}
+}
+
+func (it *Interactor) CreateUser(email string, name string, password string) (string, error) {
+	_, err := it.UserRepository.FindByEmail(email)
+	switch err {
+	case nil:
+		return "", FailedCreateUser
+	case UserNotFoundError:
+
+	default:
+		return "", err
+	}
+	return it.UserRepository.Create(email, name, password)
+}
