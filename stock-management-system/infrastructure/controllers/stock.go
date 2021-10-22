ba6121fa610f085dc6fa074b751801b6e3a17035
endered <yy56ga10ve@gmail.com>
2021-10-23 02:29:09 +0900

    add change stock amount method

--- stock-management-system/infrastructure/controllers/stock.go
+++ stock-management-system/infrastructure/controllers/stock.go
@@ -7,3 +7,11 @@ func (c *Controller) GetStockALL() (int, string, error) {
 	}
 	return jsonDump(stocks)
 }
+
+func (c *Controller) ChangeStockAmount(id string, add int) (int, string, error) {
+	err := c.Interactor.ChangeStockAmount(id, add)
+	if err != nil {
+		return 404, "Failed to change amount", nil
+	}
+	return jsonDump(map[string]string{"message": "successful change!"})
+}
