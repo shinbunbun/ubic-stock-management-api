e7689213ae7e6b2a56afdbe71956a51b98511e26
endered <yy56ga10ve@gmail.com>
2021-10-23 02:51:04 +0900

    refactoring code

--- stock-management-system/infrastructure/controllers/stock.go
+++ stock-management-system/infrastructure/controllers/stock.go
@@ -3,7 +3,7 @@ package controllers
 func (c *Controller) GetStockALL() (int, string, error) {
 	stocks, err := c.Interactor.FindStockAll()
 	if err != nil {
-		return 404, "getStockALL(): error", nil
+		return internalErrorMessage("Failed to get stocks")
 	}
 	return jsonDump(stocks)
 }
@@ -11,7 +11,7 @@ func (c *Controller) GetStockALL() (int, string, error) {
 func (c *Controller) ChangeStockAmount(id string, add int) (int, string, error) {
 	err := c.Interactor.ChangeStockAmount(id, add)
 	if err != nil {
-		return 404, "Failed to change amount", nil
+		return internalErrorMessage("Failed to change amount!")
 	}
 	return message("successful change amount!")
 }
@@ -19,7 +19,7 @@ func (c *Controller) ChangeStockAmount(id string, add int) (int, string, error) 
 func (c *Controller) DeleteStock(id string) (int, string, error) {
 	err := c.Interactor.DeleteStock(id)
 	if err != nil {
-		return 404, "Failed to delete stock", nil
+		return internalErrorMessage("Failed to delete stock")
 	}
 	return message("successful delete stock!")
 }
