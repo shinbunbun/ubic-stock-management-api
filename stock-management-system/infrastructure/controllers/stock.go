32a86534d11b8866747638888b22fd00b1893e19
endered <yy56ga10ve@gmail.com>
2021-10-23 03:16:35 +0900

    add create stock method

--- stock-management-system/infrastructure/controllers/stock.go
+++ stock-management-system/infrastructure/controllers/stock.go
@@ -1,5 +1,7 @@
 package controllers
 
+import "github.com/Yuto/ubic-stock-management-api/stock-management-system/domain"
+
 func (c *Controller) GetStockALL() (int, string, error) {
 	stocks, err := c.Interactor.FindStockAll()
 	if err != nil {
@@ -22,6 +24,21 @@ func (c *Controller) ChangeStockAmount(id string, add int) (int, string, error) 
 		return internalErrorMessage("Failed to change amount!")
 	}
 	return message("successful change amount!")
+}
+
+func (c *Controller) CreateStock(image string, makerName string, productName string, amount int) (int, string, error) {
+	id, err := c.Interactor.CreateStock(image, makerName, productName, amount)
+	if err != nil {
+		return internalErrorMessage("Failed to create stock")
+	}
+	res := domain.Stock{
+		ID:          id,
+		Image:       image,
+		MakerName:   makerName,
+		Amount:      amount,
+		ProductName: productName,
+	}
+	return jsonDump(res)
 }
 
 func (c *Controller) DeleteStock(id string) (int, string, error) {
